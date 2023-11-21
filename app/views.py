a8c5e06ca0780d217555c1bb81c794f9d5055abf
divyadhathri <divyadhatri.b@gmail.com>
2023-11-21 14:59:01 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+# Create your views here.
+def sai(request):
+    return render(request,'sai.html')
+def divya(request):
+    return render(request,'divya.html')
